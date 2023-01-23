9d04ef626eb4c40689866a423f2b26903e5661d3
Ilya Voggov <voggov1@mail.ru>
2023-01-23 03:13:58 +0300

    Lab4

--- app/src/main/java/com/example/lab1/SecondActivity.java
+++ app/src/main/java/com/example/lab1/SecondActivity.java
@@ -1,7 +1,11 @@
 package com.example.lab1;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -9,25 +13,31 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static Button buttonAdd;
     private static Button buttonDel;
-    private static ArrayList<String> myStringArray;
-    private static ArrayList<String> selectStringArray;
+    private static List<String> myStringArray;
+    private static List<String> selectStringArray;
     private static ArrayAdapter<String> textAdapter;
     private static ListView listView;
     private static TextView textView;
+    public static SharedPreferences sharedPreferences;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        sharedPreferences = getPreferences(MODE_PRIVATE);
         setContentView(R.layout.activity_second);
         listView = findViewById(R.id.textList);
         myStringArray = new ArrayList<>();
@@ -49,6 +59,7 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
             }
         });
 
+
     }
 
     @Override
@@ -62,6 +73,37 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
             default:
                 break;
         }
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putStringSet("List", myStringArray.stream().collect(Collectors.toSet()));
+        editor.apply();
+        Log.e("jopa", "pizda");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Set<String> setString = new HashSet<>();
+        setString = sharedPreferences.getStringSet("List", setString);
+        if (!setString.isEmpty()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+               /* myStringArray = setString.stream().collect(Collectors.toList());
+                textAdapter.notifyDataSetChanged();*/
+                for (String i : setString) {
+                    add(i);
+                }
+
+            }
+        }
+        Log.e("govno", "penis");
+
+
 
     }
 
